85b4e13b8e95fad2cc8defa57a08bb3821b7fac9
angelaMinardi <168023319+angelaMinardi@users.noreply.github.com>
2024-11-09 22:38:16 -0500

    Update options.js

--- options.js
+++ options.js
@@ -1,42 +1,5 @@
 // options.js
 
-document.getElementById('fontUpload').addEventListener('change', function () {
-  const file = this.files[0];
-  if (file) {
-    const validTypes = [
-      'font/ttf', 'font/otf', 'font/woff', 'font/woff2',
-      'application/x-font-ttf', 'application/x-font-otf',
-      'application/octet-stream', ''
-    ];
-    if (!validTypes.includes(file.type)) {
-      alert('Unsupported font type.');
-      return;
-    }
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      const fontData = e.target.result;
-      const fontName = file.name.replace(/\.[^/.]+$/, "");
-      const fontFormat = file.type.split('/')[1] || 'truetype';
-
-      const fontFace = `
-        @font-face {
-          font-family: '${fontName}';
-          src: url('${fontData}') format('${fontFormat}');
-        }
-      `;
-
-      // Save font data to storage
-      browser.storage.local.set({
-        customFontFace: fontFace,
-        customFontName: fontName
-      }).then(() => {
-        alert('Custom font applied!');
-      });
-    };
-    reader.readAsDataURL(file);
-  }
-});
-
 document.getElementById('useAurebesh').addEventListener('click', function () {
   // Define multiple font faces for Aurebesh variants
   const fontFaces = `
@@ -83,11 +46,5 @@ document.getElementById('useAurebesh').addEventListener('click', function () {
     customFontName: 'Aurebesh'
   }).then(() => {
     alert('Aurebesh font applied!');
-  });
-});
-
-document.getElementById('resetFont').addEventListener('click', function () {
-  browser.storage.local.remove(['customFontFace', 'customFontName']).then(() => {
-    alert('Font reset to default.');
   });
 });
