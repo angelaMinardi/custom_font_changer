fd7a2a63b62be0285cd5453574801f5106c415e3
angelaMinardi <168023319+angelaMinardi@users.noreply.github.com>
2024-11-09 22:41:15 -0500

    Update options.js

--- options.js
+++ options.js
@@ -1,45 +1,38 @@
 // options.js
 
-document.getElementById('useAurebesh').addEventListener('click', function () {
-  // Define multiple font faces for Aurebesh variants
-  const fontFaces = `
-    @font-face {
-      font-family: 'Aurebesh';
-      src: url('${browser.runtime.getURL('Aurebesh.otf')}') format('opentype');
-      font-weight: normal;
-      font-style: normal;
-    }
-    @font-face {
-      font-family: 'Aurebesh';
-      src: url('${browser.runtime.getURL('Aurebesh Italic.otf')}') format('opentype');
-      font-weight: normal;
-      font-style: italic;
-    }
-    @font-face {
-      font-family: 'Aurebesh';
-      src: url('${browser.runtime.getURL('Aurebesh Bold.otf')}') format('opentype');
-      font-weight: bold;
-      font-style: normal;
-    }
-    @font-face {
-      font-family: 'Aurebesh';
-      src: url('${browser.runtime.getURL('Aurebesh Bold Italic.otf')}') format('opentype');
-      font-weight: bold;
-      font-style: italic;
-    }
-    @font-face {
-      font-family: 'Aurebesh Condensed';
-      src: url('${browser.runtime.getURL('Aurebesh Condensed.otf')}') format('opentype');
-      font-weight: normal;
-      font-style: normal;
-    }
-    @font-face {
-      font-family: 'Aurebesh Condensed';
-      src: url('${browser.runtime.getURL('Aurebesh Condensed Italic.otf')}') format('opentype');
-      font-weight: normal;
-      font-style: italic;
-    }
-  `;
+function arrayBufferToBase64(buffer) {
+  let binary = '';
+  let bytes = new Uint8Array(buffer);
+  bytes.forEach((b) => binary += String.fromCharCode(b));
+  return window.btoa(binary);
+}
+
+document.getElementById('useAurebesh').addEventListener('click', async function () {
+  const fontFiles = [
+    { name: 'Aurebesh', file: 'Aurebesh.otf', weight: 'normal', style: 'normal' },
+    { name: 'Aurebesh', file: 'Aurebesh Bold.otf', weight: 'bold', style: 'normal' },
+    { name: 'Aurebesh', file: 'Aurebesh Italic.otf', weight: 'normal', style: 'italic' },
+    { name: 'Aurebesh', file: 'Aurebesh Bold Italic.otf', weight: 'bold', style: 'italic' },
+    { name: 'Aurebesh Condensed', file: 'Aurebesh Condensed.otf', weight: 'normal', style: 'normal' },
+    { name: 'Aurebesh Condensed', file: 'Aurebesh Condensed Italic.otf', weight: 'normal', style: 'italic' },
+  ];
+
+  let fontFaces = '';
+
+  for (const font of fontFiles) {
+    const response = await fetch(browser.runtime.getURL(font.file));
+    const arrayBuffer = await response.arrayBuffer();
+    const base64Font = arrayBufferToBase64(arrayBuffer);
+
+    fontFaces += `
+      @font-face {
+        font-family: '${font.name}';
+        src: url('data:font/otf;base64,${base64Font}') format('opentype');
+        font-weight: ${font.weight};
+        font-style: ${font.style};
+      }
+    `;
+  }
 
   browser.storage.local.set({
     customFontFace: fontFaces,
