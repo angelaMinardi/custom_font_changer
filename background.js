8bdc319349113835a134df2a87ff62df27018899
angelaMinardi <168023319+angelaMinardi@users.noreply.github.com>
2024-11-09 22:30:09 -0500

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-// background.js
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete') {
-    browser.scripting.executeScript({
-      target: { tabId: tabId },
-      files: ['contentScript.js']
-    });
-  }
-});
