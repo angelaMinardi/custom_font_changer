9b155a3d144013c4731e45b45b48dfe36cd7b6bb
angelaMinardi <168023319+angelaMinardi@users.noreply.github.com>
2024-11-09 22:25:05 -0500

    Update options.js

--- options.js
+++ options.js
@@ -1,13 +1,19 @@
-document.getElementById('fontUpload').addEventListener('change', function() {
+// options.js
+
+document.getElementById('fontUpload').addEventListener('change', function () {
   const file = this.files[0];
   if (file) {
-    const validTypes = ['font/ttf', 'font/otf', 'font/woff', 'font/woff2', 'application/octet-stream'];
+    const validTypes = [
+      'font/ttf', 'font/otf', 'font/woff', 'font/woff2',
+      'application/x-font-ttf', 'application/x-font-otf',
+      'application/octet-stream', ''
+    ];
     if (!validTypes.includes(file.type)) {
       alert('Unsupported font type.');
       return;
     }
     const reader = new FileReader();
-    reader.onload = function(e) {
+    reader.onload = function (e) {
       const fontData = e.target.result;
       const fontName = file.name.replace(/\.[^/.]+$/, "");
       const fontFormat = file.type.split('/')[1] || 'truetype';
@@ -31,22 +37,56 @@ document.getElementById('fontUpload').addEventListener('change', function() {
   }
 });
 
-document.getElementById('useAurebesh').addEventListener('click', function() {
-  const aurebeshFontFace = `
+document.getElementById('useAurebesh').addEventListener('click', function () {
+  // Define multiple font faces for Aurebesh variants
+  const fontFaces = `
     @font-face {
       font-family: 'Aurebesh';
-      src: url('${browser.runtime.getURL('aurebesh.ttf')}') format('truetype');
+      src: url('${browser.runtime.getURL('Aurebesh.otf')}') format('opentype');
+      font-weight: normal;
+      font-style: normal;
+    }
+    @font-face {
+      font-family: 'Aurebesh';
+      src: url('${browser.runtime.getURL('Aurebesh Italic.otf')}') format('opentype');
+      font-weight: normal;
+      font-style: italic;
+    }
+    @font-face {
+      font-family: 'Aurebesh';
+      src: url('${browser.runtime.getURL('Aurebesh Bold.otf')}') format('opentype');
+      font-weight: bold;
+      font-style: normal;
+    }
+    @font-face {
+      font-family: 'Aurebesh';
+      src: url('${browser.runtime.getURL('Aurebesh Bold Italic.otf')}') format('opentype');
+      font-weight: bold;
+      font-style: italic;
+    }
+    @font-face {
+      font-family: 'Aurebesh Condensed';
+      src: url('${browser.runtime.getURL('Aurebesh Condensed.otf')}') format('opentype');
+      font-weight: normal;
+      font-style: normal;
+    }
+    @font-face {
+      font-family: 'Aurebesh Condensed';
+      src: url('${browser.runtime.getURL('Aurebesh Condensed Italic.otf')}') format('opentype');
+      font-weight: normal;
+      font-style: italic;
     }
   `;
+
   browser.storage.local.set({
-    customFontFace: aurebeshFontFace,
+    customFontFace: fontFaces,
     customFontName: 'Aurebesh'
   }).then(() => {
     alert('Aurebesh font applied!');
   });
 });
 
-document.getElementById('resetFont').addEventListener('click', function() {
+document.getElementById('resetFont').addEventListener('click', function () {
   browser.storage.local.remove(['customFontFace', 'customFontName']).then(() => {
     alert('Font reset to default.');
   });
